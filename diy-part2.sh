b0845c660fbbb8af346baae9804a2ef9793b51f7
tcsr200722 <350524166@qq.com>
2020-12-05 19:12:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,15 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+add luci-app-filebrowser
+git clone https://github.com/tcsr200722/FileBrowser.git package/luci-app-filebrowser
+add luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+add openwrt-upx
+git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+add luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+./scripts/feeds update -a
+./scripts/feeds install -a
